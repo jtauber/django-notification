251ab26b9f0edf71fceae8761c3c3d4486b5c53e
Matías Herranz matiasherranz@gmail.com <matiasherranz@gmail.com>
2012-09-28 19:18:29 -0300

    PEP8 fixes.

--- notification/context_processors.py
+++ notification/context_processors.py
@@ -3,8 +3,8 @@ from notification.models import Notice
 
 def notification(request):
     if request.user.is_authenticated():
-        return {
-            "notice_unseen_count": Notice.objects.unseen_count_for(request.user, on_site=True),
-        }
+        unseen_count = Notice.objects.unseen_count_for(request.user,
+                                                       on_site=True)
+        return {"notice_unseen_count": unseen_count}
     else:
         return {}
